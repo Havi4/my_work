3e5b160e4bd7cfb39834244f64a70416dd5bc615
Havili <1058695900@qq.com>
2014-07-02 11:05:02 +0800

    Initial Commit

---
+++ excerse/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  excerse
+//
+//  Created by Apple MBP on 14-7-2.
+//  Copyright (c) 2014年 Apple MBP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ excerseTests/excerseTests.m
@@ -0,0 +1,34 @@
+//
+//  excerseTests.m
+//  excerseTests
+//
+//  Created by Apple MBP on 14-7-2.
+//  Copyright (c) 2014年 Apple MBP. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface excerseTests : XCTestCase
+
+@end
+
+@implementation excerseTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
